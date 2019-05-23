85a8d783eb6629ef10706b4948cbcbc71a2cf4c1
Emna Trabelsi <etrabelsi@baylibre.com>
2019-05-23 15:31:18 +0200

    ii

--- work/mesure.sh
+++ work/mesure.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-while (lava-wait test_finished)
 do
-./pyacmecapture.py --ip 10.65.34.1 -s 8 -o test4 -od .
+./pyacmecapture/pyacmecapture.py --ip 10.65.34.1 -s 8 -o test4 -od .
+if (lava-wait test_finished)
+then 
+	break
+fi
 done
 ~     
